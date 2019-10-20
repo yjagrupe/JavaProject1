13b9fc7c4f05431ce34963e4b052259d8ac6e2c1
user1 <user1@test.com>
2019-10-20 21:16:22 +0530

    this is 1st commit

---
+++ src/package1/Class1.java
@@ -0,0 +1,9 @@
+package package1;
+
+public class Class1 {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        int c = 10+20;
+        System.out.println("Value of C  = "+c);
+    }
+}
