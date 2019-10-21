5744531b877b0c080103c3497998b6ccbb0e7ea7
user1 <user1@test.com>
2019-10-21 16:37:33 +0530

    this is 4th commit

--- src/package1/Class2.java
+++ src/package1/Class2.java
@@ -1,5 +1,8 @@
 package package1;
 
 public class Class2 {
-  System.out.println("Hello class 3");
+    public static void main(String[] args) {
+        System.out.println("Hello class 2");
+    }
+
 }
