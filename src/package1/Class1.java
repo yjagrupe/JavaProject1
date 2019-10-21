797d4896d876607063d02307964385314caeff8c
yjagrupe <32682704+yjagrupe@users.noreply.github.com>
2019-10-21 21:24:31 +0530

    set value of C as 200

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,7 +3,7 @@ package package1;
 public class Class1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        int c = 100;
+        int c = 200;
         System.out.println("Value of C  = "+c);
     }
 }
