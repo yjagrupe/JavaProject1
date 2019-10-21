6581548428ff6db145c3e396f71f2f6485d44d84
yjagrupe <32682704+yjagrupe@users.noreply.github.com>
2019-10-21 21:29:58 +0530

    set value of c as a+b

--- src/package1/Class1.java
+++ src/package1/Class1.java
@@ -3,7 +3,7 @@ package package1;
 public class Class1 {
     public static void main(String[] args) {
         System.out.println("Hello world");
-        int c = 200;
+        int c = a+b;
         System.out.println("Value of C  = "+c);
     }
 }
